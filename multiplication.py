2329be1a5881c6ebcc04bc0c2bcae1b81d358e60
unknown <ranjithsaddi224@gmail.com>
2022-07-18 12:43:39 +0530

    message

--- multiplication.py
+++ multiplication.py
@@ -1,6 +1,4 @@
-number=int(input("enter the number of which the user wants to print the multiplication table: "))      
-# We are using "for loop" to iterate the multiplication 10 times       
-print ("The Multiplication Table of: ", number)    
-for count in range(1, 11):      
-   print (number, 'x', count, '=', number * count)    
-
+number=int(input("enter the number of which the user wants  to print the multipliation table"))
+print("the multiplication table of:",number)
+for count in range(1,11):
+    print (number,'x',count,'=',number*count)
